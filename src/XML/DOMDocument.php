4bf3f918c4f418585cf8845e8e0d67054b655ceb
pavex <pavex@ines.cz>
2018-05-15 20:41:56 +0200

    Initial commit

---
+++ src/XML/DOMDocument.php
@@ -0,0 +1,81 @@
+<?php
+
+	namespace Publixe\XML;
+	use Publixe;
+
+
+/**
+ * DOM Document derivate with extra methods:
+ *	- getElementByClassName()
+ * 	- getInnerHTML()
+ *
+ * @author	Pavex
+ */
+	class DOMDocument extends \DOMDocument
+	{
+
+
+
+
+
+/**
+ * @param Array<Element>
+ * @param Element
+ * @param string
+ */
+		private function findElementByClass(&$elements, $element, $className)
+		{
+			if (isset($element -> tagName)) {
+				$class = $element -> getAttribute('class');
+				if ($class == $className) {
+					$elements[] = $element;
+					return;
+				}
+				if ($element -> hasChildNodes()) {
+					foreach ($element -> childNodes as $child) {
+						$this -> findElementByClass($elements, $child, $className);
+					}
+				}
+			}
+		}
+
+
+
+
+
+/**
+ * @param string
+ * @return Array<Element>
+ */
+		public function getElementByClassName($className)
+		{
+			$elements = [];
+			$this -> findElementByClass($elements, $this -> documentElement, $className);
+			return $elements;
+		}
+
+
+
+
+
+/**
+ * Great tip from Haim Evgi
+ * https://stackoverflow.com/questions/2087103/how-to-get-innerhtml-of-domnode
+ * @param DOMNode
+ * @return string
+ */
+		public function getInnerHTML($element) 
+		{
+			$innerHTML = ''; 
+			$children = $element -> childNodes;
+			foreach ($children as $child) { 
+				$innerHTML .= $element -> ownerDocument -> saveHTML($child);
+			}
+			return $innerHTML; 
+		}
+
+
+
+
+
+	}
